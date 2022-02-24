1ec9cb72d4d4f1054a89408bc3a861d646f24152
Jeff Sulfranc <sulfranc@gmail.com>
2022-02-23 19:04:21 -0500

    commit tree f7cc9264b055847b0658730e2f94269cb365ad50

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 class Formatter {
-  //add static methods here
+  static capitalize (word){
+    return word.charAt(0).toUpperCase() + word.slice(1)
+  }
+  static sanitize (word){
+    return word.replace(/[^A-Za-z0-9-' ']/g, '');
+
+  } 
+  static titleize(string) {
+    const words = string.split(' ')
+    words[0] = this.capitalize(words[0])
+    const newWords = words.map(word => {
+      if (word !== 'the' && word !== 'a' && word !== 'an' && word !== 'but'  && word !== 'of' && word !== 'and' && word !== 'for' && word !== 'at' && word !== 'by' && word !== 'from') {
+        return word = this.capitalize(word)
+        debugger
+      }
+      else {
+        return word
+      }
+    })
+    return newWords.join(' ')
+
+  }
 }
